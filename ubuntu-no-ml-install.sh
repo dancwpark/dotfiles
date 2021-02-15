ae3d67ad36e8d5b676f2a330f07c740c7bd9b4a2
Daniel Park <dancwpark@gmail.com>
2021-02-14 19:23:42 -0500

    Add 'mkdir .config' and change vim->nvim

--- ubuntu-no-ml-install.sh
+++ ubuntu-no-ml-install.sh
@@ -25,17 +25,18 @@ curl -sL install-node.now.sh/lts | sudo bash
 
 # vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir ~/.config
 cp -r nvim/ ~/.config/nvim/
 vim +'PlugInstall | PlugUpdate | PlugUpgrade | q | q'
 
 # coc.nvim extensions
-vim +'CocInstall coc-json | qall'
-vim +'CocInstall coc-clangd | qall'
-vim +'CocInstall coc-css | qall'
-vim +'CocInstall coc-git | qall'
-vim +'CocInstall coc-html | qall'
-vim +'CocInstall coc-java | qall'
-vim +'CocInstall coc-jedi | qall'
+nvim +'CocInstall coc-json | qall'
+nvim +'CocInstall coc-clangd | qall'
+nvim +'CocInstall coc-css | qall'
+nvim +'CocInstall coc-git | qall'
+nvim +'CocInstall coc-html | qall'
+nvim +'CocInstall coc-java | qall'
+nvim +'CocInstall coc-jedi | qall'
 
 # jedi (for coc-jedi)
 python3 -m pip install jedi-language-server
