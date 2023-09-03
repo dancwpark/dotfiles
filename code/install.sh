250437c7e69e8a8baa90477e53da2e0f713265bd
dancwpark <dancwpark@gmail.com>
2023-09-02 20:44:40 -0400

    vscode install extensions -- it's a start

---
+++ code/install.sh
@@ -0,0 +1,11 @@
+code --install-extension visualstudioexptteam.vscodeintellicode
+code --install-extension oderwat.indent-rainbow
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension eamodio.gitlens
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension garytyler.darcula-pycharm
+code --install-extension iterative.dvc
+code --install-extension ms-python.python
+
+
